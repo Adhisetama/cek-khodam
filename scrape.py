6bfb406636776f346f0fd1395b2ff38025aa1fb5
Adhisetama <adhisetama@gmail.com>
2024-06-22 01:09:57 +0700

    fix bug when printing emojis

--- scrape.py
+++ scrape.py
@@ -16,7 +16,9 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # buat nyimpen khodam
 khodams = set()
-iterations = 100
+iterations = 1000
+
+
 
 try:
     url = 'https://check-your-khodam.vercel.app'
@@ -29,23 +31,28 @@ try:
         text_input.send_keys('hirasawa yui')
 
         # 2. Klik tombol cek khodam
-        submit_button = driver.find_element(By.ID, 'btn-check')  # Update with actual element ID
+        submit_button = driver.find_element(By.ID, 'btn-check')
         submit_button.click()
 
         # 3. Tunggu sampe dapet khodam
         wait = WebDriverWait(driver, 10)
-        div_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'result-text')))  # Update with actual element ID
+        div_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'result-text')))
 
         # 4. Ambil teksnya
         result_text = div_element.text
 
-        print(i, result_text)
+        # 5. Print & save
+        try:
+            print(i, result_text)
+        except UnicodeEncodeError:
+            print(i, '<error bang gabisa ngeprint emoji>')
         khodams.add(result_text)
 
         driver.refresh()
+
 finally:
     driver.quit()
 
-with open('khodam.txt', 'w') as khodam_file:
-    for khodam in khodams:
-        khodam_file.write(f'{khodam}\n')
+    with open('khodam.txt', 'w', encoding='unicode_escape') as khodam_file:
+        for khodam in khodams:
+            khodam_file.write(f'{khodam}\n')
